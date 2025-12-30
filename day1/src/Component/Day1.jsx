b8f3e8b8cb2363510bbdc82aa6398f366b1b3501
Anishma Galami <anishmagalami@Anishma-2.local>
2025-12-30 21:24:05 +0545

    first commit

---
+++ day1/src/Component/Day1.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import "./Day1.css";
+import { resolveConfig } from 'vite';
+
+function Day1() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increment
+      </button>
+
+      <button onClick={() => setCount(count - 1)}>
+        Decrement
+      </button>
+
+      <button onClick={() => setCount(0)}>
+        Reset
+      </button>
+    </div>
+  );
+}
+
+export default Day1;
